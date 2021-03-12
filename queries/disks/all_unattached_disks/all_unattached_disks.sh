051c5a4e1ef4fb91eb669e322d4c66cba3020548
jay.jain <jay.jain@jvion.com>
2021-03-12 15:56:45 -0500

    added more options to all_unattached_disks script

--- queries/disks/all_unattached_disks/all_unattached_disks.sh
+++ queries/disks/all_unattached_disks/all_unattached_disks.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-az graph query -q 'where type =~ "Microsoft.Compute/disks" | where properties.diskState=="Unattached" | project Name = name, DiskState = properties.diskState, OSType = properties.osType ' -o table
+az graph query -q 'where type =~ "Microsoft.Compute/disks"
+| where properties.diskState=="Unattached"
+| project Name = name, 
+            OSType = properties.osType, 
+            SkuTier = sku.tier, 
+            DiskSize = toint(properties.diskSizeGB),
+            Location = location
+| order by DiskSize
+' \
+-o table
